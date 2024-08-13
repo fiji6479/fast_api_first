0fb827745cfad555c10b11c3a48aa894a03a61b8
fiji6479 <71012423+fiji6479@users.noreply.github.com>
2024-08-13 19:10:56 +0300

    Update run.py

--- game_blog/run.py
+++ game_blog/run.py
@@ -2,8 +2,7 @@ from uvicorn import run
 from main import create_app
 
 
-
 app = create_app()
 
 if __name__ == '__main__':
-    uvicorn.run('run:app', host='127.0.0.1', port=5000, reload=True)
+    run('run:app', host='127.0.0.1', port=5000, reload=True)
